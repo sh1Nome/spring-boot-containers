b96352dc30e05b9666ba90347d23c21b16e15b41
sh1Nome <shinodayo1024@gmail.com>
2024-03-27 23:05:31 +0900

    [update]

--- prod/mount/entrypoint.sh
+++ prod/mount/entrypoint.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 function start() {
+    # MySQLの起動確認
+    while true
+    do
+        nc -vz db 3306
+        if [ $? = 0 ]; then
+            break
+        fi
+        sleep 1
+    done
+
+    # SpringBootアプリケーションの起動
     JAR_NAME=`ls /opt/app | grep jar`
     java -jar /opt/app/${JAR_NAME}
 }
