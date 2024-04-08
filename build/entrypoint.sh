1096eec5910e14e156359e5eac39560bfc6b76e9
sh1Nome <shinodayo1024@gmail.com>
2024-04-08 22:28:12 +0900

    [remove]readmeのみに

--- build/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/workuser/work/ARTIFACT_NAME
-./gradlew build
-cp ./build/libs/* ../build
-echo 'build finished'
-
-trap 'exit 0' TERM
-tail -f /dev/null & wait $!
--- dev/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-function shutdown() {
-    echo 'stop container'
-    exit 0
-}
-trap shutdown TERM
-tail -f /dev/null & wait $!
